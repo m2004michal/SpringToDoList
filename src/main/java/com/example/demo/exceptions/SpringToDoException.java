66f761e01e03a19f4bf6211034ef741c93312653
micha <michal.tusz12@gmail.com>
2021-12-19 20:31:38 +0100

    initial commit with models and repositories

---
+++ src/main/java/com/example/demo/exceptions/SpringToDoException.java
@@ -0,0 +1,5 @@
+package com.example.demo.exceptions;
+
+public class SpringToDoException extends RuntimeException{
+    public SpringToDoException(String exMessage){super(exMessage);}
+}
---
+++ src/main/java/com/example/demo/model/NotificationEmail.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NotificationEmail {
+    private String recipient;
+    private String subject;
+    private String body;
+}
