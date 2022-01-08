e91865e98cee8ed0576a10a5b4f1a2aa84bd9818
micha <michal.tusz12@gmail.com>
2022-01-08 14:53:20 +0100

    implemented swagger

--- src/main/java/com/example/SpringToDoApp/SpringBootToDoListApplication.java
+++ src/main/java/com/example/SpringToDoApp/SpringBootToDoListApplication.java
@@ -1,11 +1,14 @@
 package com.example.SpringToDoApp;
 
+import com.example.SpringToDoApp.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class SpringBootToDoListApplication {
 
 	public static void main(String[] args) {
